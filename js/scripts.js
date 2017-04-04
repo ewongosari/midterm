a10c279353e1c95449fc9ee0f8a4cbdd3bed8d6f
Evelina Wongosari <ew2537@tc.columbia.edu>
2017-04-04 15:40:14 -0400

    added quizzes, fixed modals, finished instrument content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,328 @@
-// Launch Pop-Up Video (still not working)
-//$('#videoLink').click(function () {
-    var src = '';
-    $('#modal-video iframe').attr('src', src);
-});
-// End Pop-Up Video when modal is closed
-//$('#modal-video button.close').on('hidden.bs.modal', function () {
-    $('#modal-video iframe').removeAttr('src');
-});
+//Pre-Assessment Quiz
+(function() {
+  var questions = [{
+    question: "There are five families of instruments in the orchestra.",
+    choices: ["True", "False"],
+    correctAnswer: 1
+  }, {
+    question: "All of the woodwind instruments are made of wood.",
+    choices: ["True", "False"],
+    correctAnswer: 1
+  }, {
+    question: "The piano is part of the percussion family.",
+    choices: ["True", "False"],
+    correctAnswer: 0
+  }];
+
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#prequiz'); //Quiz div object
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the "next" button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('You have not choosen an answer. Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the "prev" button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+
+  // Click handler for the "Start Over" button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        }
+
+        // Controls display of "prev" button
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+
+    score.append('You got ' + numCorrect + ' questions correct out of a total of ' +
+                 questions.length + '.');
+    return score;
+  }
+})();
+
+
+//Post-Assessment Quiz
+(function() {
+  var questions = [{
+    question: "The harp has _____ strings stretched across its body.",
+    choices: ["21", "45", "15", "7"],
+    correctAnswer: 1
+  }, {
+    question: "The _____ is primarily played while sitting down on a high chair or standing up.",
+    choices: ["double bass", "violin", "cello", "viola"],
+    correctAnswer: 0
+  }, {
+    question: "A woodwind instrument can be categorized by having a _____ mouthpiece.",
+    choices: ["flute", "single reed", "double reed", "All of the above"],
+    correctAnswer: 3
+  }, {
+    question: "The _____ horn is part of the brass family.",
+    choices: ["English", "French", "Italian", "American"],
+    correctAnswer: 1
+  }, {
+    question: "Only a musician in the _____ group can play multiple instruments.",
+    choices: ["brass", "string", "percussion", "woodwind"],
+    correctAnswer: 2
+  }];
+
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#postquiz'); //Quiz div object
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the "next" button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('You have not choosen an answer. Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the "prev" button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+
+  // Click handler for the "Start Over" button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        }
+
+        // Controls display of "prev" button
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+
+    score.append('You got ' + numCorrect + ' questions correct out of a total of ' +
+                 questions.length + '.');
+    return score;
+  }
+})();
